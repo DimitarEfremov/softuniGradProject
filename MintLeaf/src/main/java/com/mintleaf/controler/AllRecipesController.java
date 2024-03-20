e7eb448105449212bb12876f3a415f59c3d44e55
DimitarEfremov <dimitar_efremov@abv.bg>
2024-03-20 16:22:45 +0200

    Almost fixed the page with all recipes

--- MintLeaf/src/main/java/com/mintleaf/controler/AllRecipesController.java
+++ MintLeaf/src/main/java/com/mintleaf/controler/AllRecipesController.java
@@ -4,11 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 @Controller
-public class AllRecipes {
+public class AllRecipesController {
 
     @GetMapping("/recipes")
     public ModelAndView allRecipes(){
 
         return new ModelAndView("allRecipes");
     }
+
 }
