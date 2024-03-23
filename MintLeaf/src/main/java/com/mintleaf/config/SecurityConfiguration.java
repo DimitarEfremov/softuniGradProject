d7f55604391c53d20ee81c02c5bdc490d194b89f
DimitarEfremov <dimitar_efremov@abv.bg>
2024-03-23 16:16:12 +0200

    started working on login and security

--- MintLeaf/src/main/java/com/mintleaf/config/SecurityConfiguration.java
+++ MintLeaf/src/main/java/com/mintleaf/config/SecurityConfiguration.java
@@ -3,19 +3,41 @@ package com.mintleaf.config;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity){
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 
-            httpSecurity.authorizeHttpRequests(
-                    authoriseRequests -> authoriseRequests
-                            .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                            .requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll()
-            )
+        httpSecurity.authorizeHttpRequests(
+                authoriseRequests -> authoriseRequests
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers("/", "/register", "/login", "/allRecipes").permitAll()
+                        .anyRequest().authenticated()
+        ).formLogin(
+                formLogin -> {
+                    formLogin
+                            .loginPage("/login")
+                            .usernameParameter("username")
+                            .passwordParameter("password")
+                            .defaultSuccessUrl("/")
+                            .failureUrl("/login");
+                }
+        ).logout(
+                logout -> {
+                    logout.logoutUrl("/users/logout")
+                            .logoutSuccessUrl("/")
+                            .invalidateHttpSession(true);
+                }
+        );
+
+        return httpSecurity.build();
     }
 }
